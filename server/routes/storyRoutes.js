3d20c8a5b10cb35b718954ecaac3e98613c03e96
saniya8985 <saniyasharmaetah@gmail.com>
2026-04-21 14:08:53 +0530

    project completed

--- server/routes/storyRoutes.js
+++ server/routes/storyRoutes.js
@@ -3,7 +3,6 @@ import { upload } from '../configs/multer.js';
 import { addUserStory, getStories } from '../controllers/storyController.js';
 import { protect } from '../middlewares/auth.js';
 
-
 const storyRouter = express.Router();
 
 storyRouter.post('/create', upload.single('media'), protect, addUserStory)
